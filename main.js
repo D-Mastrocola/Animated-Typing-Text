55629a6a7c4ce43e41988767c5a2f4e9a720467d
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2020-12-11 22:08:30 -0500

    Typing cursor
    Added a cursor that moves across the screen to look like somebody is typing

--- main.js
+++ main.js
@@ -3,28 +3,48 @@ const text = document.querySelector('#letters').children;
 var textArray = [];
 
 //Seperate each row so I can retain format
-for(let i = 0; i < text.length; i++) {
+for (let i = 0; i < text.length; i++) {
   textArray.push(text[i]);
 }
 
 //Seperate each letter with their own span tag using regex
-for(let i = 0; i < textArray.length; i++) {
+for (let i = 0; i < textArray.length; i++) {
   textArray[i].innerHTML = textArray[i].textContent.replace(
     /./g,
     "<span class='letter'>$&</span>"
   );
   console.log("replaced")
 }
-
+console.log(document.querySelector('#letters').getBoundingClientRect().x)
 //Changes the cursor height based on the text size;
-//document.querySelector('.cursor').style.height = document.querySelector('.message').offsetHeight + "px";
+document.querySelector('.cursor').style.height = document.querySelector('.letter').offsetHeight + "px";
+//Moves the cursor to the starting position
 
-//Fades in the letters one by one with a small variance in timing to create a human feel.
-  anime({
+//create keyframes for animation
+let cursorPos = [];
+let letters = textArray[1].children;
+console.log(textArray)
+for(let i = 1; i < letters.length; i++) {
+  
+  let frame = {
+    translateX: letters[i].getBoundingClientRect().x,
+    translateY: letters[i].getBoundingClientRect().y,
+    duration: 0,
+    delay: 50,
+    easing: 'linear'
+  };
+  console.log(textArray[i]);
+  cursorPos.push(frame);
+}
+console.log(cursorPos)
+//Fades in the letters one by one.
+anime.timeline()
+  .add({
     targets: ".letter",
     opacity: [0, 1],
-    duration: 1000,
-    delay: (el, i) => i * 150 + (Math.random() * 150),
-    loop: true
-  });
-  
+    delay: (el, i) => i * 50,
+  })
+  .add({
+    targets: ".cursor",
+    keyframes: cursorPos,
+  }, 0);
